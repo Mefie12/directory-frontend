9cd49437d5999cb6c4c13ba4b26e872c61b08bfc
K-0bby <k.hoenyefia@gmail.com>
2026-02-21 09:07:44 +0000

    update to api listing

--- src/app/dashboard/listing-agent/my-listing/create/form-component/business-details.tsx
+++ src/app/dashboard/listing-agent/my-listing/create/form-component/business-details.tsx
@@ -293,7 +293,7 @@ export const BusinessDetailsForm = forwardRef<ListingFormHandle, Props>(
         const detailsReq = fetch(
           `${API_URL}/api/listing/${effectiveSlug}/address`,
           {
-            method: "PATCH",
+            method: "PUT",
             headers: {
               "Content-Type": "application/json",
               Accept: "application/json",
