1152b9e154163db0c99099adc95abdfae672f6b3
K-0bby <k.hoenyefia@gmail.com>
2026-02-21 10:10:32 +0000

    update: bug fixes

--- src/app/dashboard/admin/listings/page.tsx
+++ src/app/dashboard/admin/listings/page.tsx
@@ -561,7 +561,7 @@ export default function Listings() {
       const API_URL = process.env.API_URL || "https://me-fie.co.uk";
 
       const response = await fetch(
-        `${API_URL}/api/admin_listings/${listingToDelete}`,
+        `${API_URL}/api/listing/${listingToDelete}`,
         {
           method: "DELETE",
           headers: {
@@ -604,7 +604,7 @@ export default function Listings() {
       const API_URL = process.env.API_URL || "https://me-fie.co.uk";
 
       const response = await fetch(
-        `${API_URL}/api/admin_listings/${listingSlug}/update_status`,
+        `${API_URL}/api/listing/${listingSlug}/update_status`,
         {
           method: "PATCH",
           headers: {
